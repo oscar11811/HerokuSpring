85092424182cb900961a94537307463dcfeba674
Oscar Salome <oscarsalome.developer@gmail.com>
2022-01-13 18:31:38 +0100

    first commit

---
+++ src/main/java/com/example/uf4pt1part3/Uf4Pt1Part3Application.java
@@ -0,0 +1,21 @@
+package com.example.uf4pt1part3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class Uf4Pt1Part3Application {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Uf4Pt1Part3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uf4pt1part3/Uf4Pt1Part3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uf4pt1part3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uf4Pt1Part3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
