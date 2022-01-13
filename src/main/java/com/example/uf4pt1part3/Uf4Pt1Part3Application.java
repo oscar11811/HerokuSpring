d01614b2ba8986a8e5e8fd330e121237eb0c03f7
Oscar Salome <oscarsalome.developer@gmail.com>
2022-01-13 18:43:58 +0100

    add line @Controller

--- src/main/java/com/example/uf4pt1part3/Uf4Pt1Part3Application.java
+++ src/main/java/com/example/uf4pt1part3/Uf4Pt1Part3Application.java
@@ -2,9 +2,11 @@ package com.example.uf4pt1part3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class Uf4Pt1Part3Application {
 
